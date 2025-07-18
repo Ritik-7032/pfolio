ecac570a7a43503b7339401cfe2216d14b8b552c
Ritik Kumar <155009561+Ritik-7032@users.noreply.github.com>
2025-07-18 16:11:32 +0530

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -9,14 +9,14 @@ export async function POST(req) {
 
     const body = await req.json();
     const { name, email, message } = body;
-
+     
     if (!name || !email || !message) {
       return NextResponse.json({ error: "Missing fields" }, { status: 400 });
     }
-
+   console.log("Saved in db");
     const saved = await Contact.create({ name, email, message });
 
-    
+      console.log(saved);
     await sendContactEmails({ name, email, message });
 
     return NextResponse.json(
@@ -24,7 +24,7 @@ export async function POST(req) {
       { status: 201 }
     );
   } catch (err) {
-    console.error("Contact API Error:", err);
+    console.log("Contact API Error:", err);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
