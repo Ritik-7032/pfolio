2f4e7175f0836c881c119fec4bc9e5d0b48a2104
Ritik Kumar <155009561+Ritik-7032@users.noreply.github.com>
2025-07-18 15:42:16 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
+import Contac from "@/components/Contac";
 import Education from "@/components/Education";
 
 export default function HomePage() {
